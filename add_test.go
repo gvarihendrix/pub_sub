b277893ff822f959f0d3c16e6b3e899484f695e3
Ingvar Sigurdsson <gvarihendrix@gmail.com>
2015-09-24 20:44:45 +0000

    Created a new test

--- add_test.go
+++ add_test.go
@@ -8,3 +8,10 @@ func TestAdd(t *testing.T) {
 		t.Fatalf("Adding 1 + 2 should be 3 but got: %d", ble)
 	}
 }
+
+func BadTest(t *testing.T) {
+	num := Add(2, 2)
+	if num != 5 {
+		t.Fatalf("Adding 2 + 2 should be 4 but got: %d", num)
+	}
+}
